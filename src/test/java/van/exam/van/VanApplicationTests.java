1ebf8372788c205598c2fcdf34aa4610ef36482f
van <vgalvoso@gmail.com>
2022-12-04 21:12:50 +0800

    Initial commit

---
+++ src/test/java/van/exam/van/VanApplicationTests.java
@@ -0,0 +1,13 @@
+package van.exam.van;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
